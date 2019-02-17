3cf18059a68cbe6a8f23811a4867b1ba96886b42
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-17 10:27:51 -0500

    Stuff

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
-import { Header, Input, Tile } from "react-native-elements";
-import { StyleSheet, View, Text } from "react-native";
+import { Header, Input, Tile, Card, Image, Text } from "react-native-elements";
+import { StyleSheet, View } from "react-native";
 import axios from "axios";
 
 export default class App extends React.Component {
@@ -40,16 +40,40 @@ export default class App extends React.Component {
           <Text>LOADING...</Text>
         ) : (
           <>
-            <Tile
+            <Card
+              title={`${this.state.data.location.name}, ${
+                this.state.data.location.country
+              } `}
+              titleStyle={{ display: "flex", alignItems: "center" }}
+              image={{ uri: `https:${this.state.data.current.condition.icon}` }}
+              imageWrapperStyle={{ display: "flex", alignItems: "center" }}
+              imageStyle={{ width: "40%" }}
+            >
+              <View
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                  alignItems: "center"
+                }}
+              >
+                <Text h1>{this.state.data.current.temp_f} ℉</Text>
+                <Text h4>{this.state.data.current.condition.text}</Text>
+              </View>
+            </Card>
+
+            {/* <Tile
+            
               title={`${this.state.data.location.name}, ${
                 this.state.data.location.country
               } `}
               featured
               caption={this.state.data.location.localtime}
-            />
+              style={styles.tile}
+            /> */}
             <View
               style={{
-                flex: 1,
+                display: "flex",
                 flexDirection: "row",
                 justifyContent: "space-around",
                 marginTop: 20
@@ -59,6 +83,17 @@ export default class App extends React.Component {
               <Text>{`${this.state.data.current.temp_f} F`}</Text>
               <Text>Feels Like</Text>
               <Text>{`${this.state.data.current.feelslike_f} F`}</Text>
+            </View>
+            <View
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: 40
+              }}
+            >
+              <Text h2>{this.state.data.current.wind_dir}</Text>
+              <Text>Wind Direction</Text>
+              <Text>Speed: {this.state.data.current.wind_mph} mph</Text>
             </View>
           </>
         )}
@@ -71,5 +106,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff"
+  },
+  tile: {
+    backgroundColor: "black"
   }
 });
